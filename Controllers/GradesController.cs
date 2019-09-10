7c709549b4686ae1f33a3cf2d3e9bccf8c0418fb
Megane <38261962+MDarnaud@users.noreply.github.com>
2019-09-10 11:27:16 -0400

    Add project files.

---
+++ Controllers/GradesController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sheet2.Controllers
+{
+    public class GradesController : Controller
+    {
+        [HttpGet]
+        public ActionResult GradesIndex()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GradesIndex(FormCollection data)
+        {
+            string grade1String = data["grade1"];
+            string grade2String = data["grade2"];
+            string grade3String = data["grade3"];
+
+            Double grade1 = Convert.ToDouble(grade1String);
+            Double grade2 = Convert.ToDouble(grade2String);
+            Double grade3 = Convert.ToDouble(grade3String);
+
+            String averageLetter = "null";
+            Double average = (grade1 + grade2 + grade3) / 3;
+            if (average >= 90)
+            {
+                averageLetter = "A";
+            }
+            else if (average >= 80 && average < 90)
+            {
+                averageLetter = "B";
+            }
+            else if (average >= 70 && average < 80)
+            {
+                averageLetter = "C";
+            }
+            else if (average >= 60 && average < 70)
+            {
+                averageLetter = "D";
+            }
+            else if (average < 60)
+            {
+                averageLetter = "E";
+            }
+
+            ViewBag.Grade = averageLetter;
+
+            return View();
+
+        }
+
+
+    }
+}
---
+++ Controllers/LoanController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sheet2.Controllers
+{
+    public class LoanController : Controller
+    {
+        [HttpGet]
+        public ActionResult LoanIndex()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LoanIndex(Double investment, Double interest_rate, int years)
+        {
+            interest_rate /= 100;
+            Double Money = investment;
+
+            for(int i = 0; i < years; i++) {
+                Money += (Money*interest_rate);
+            }
+            ViewBag.Money = Money;
+
+            return View();
+        }
+    }
+}
